b1ae9f4b4a2afb9ad98b9955969c85d0332e056a
iBiktop <biktopbb@gmail.com>
2018-11-22 18:13:51 +0200

    1

--- server.js
+++ server.js
@@ -12,34 +12,40 @@ app.use(express.static(__dirname + '/public'));//через express.static мы 
 
 
 app.get('/', function (req, res) {    
-    res.render('index', {objData: "Поиск..."});    
+    res.render('index', {objData: "Поиск..."}); 
+    // res.end("http://localhost:3333/");   
 });
 
-app.post('/search', function (req, res) {
-    var lat = req.body.lat;
-    var lng = req.body.lng;
+// app.post('/search', function (req, res) {
+//     var lat = req.body.lat;
+//     var lng = req.body.lng;
 
-    request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=50&key=AIzaSyAGBONIz-nYIgKCtdPt373jsPCBA42d1Fc`, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var data = JSON.parse(body)
-        console.log(data);
-        if(data.results[0]){
-            res.render('index', {objData: data.results[0].name});
-        }else{
-            res.render('index', {objData: "Что-то не так с запросом!!!"});
-        }
+//     request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=50&key=AIzaSyAGBONIz-nYIgKCtdPt373jsPCBA42d1Fc`, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         var data = JSON.parse(body)
+//         console.log(data);
+//         if(data.results[0]){
+//             res.render('index', {objData: data.results[0].name});
+//         }else{
+//             res.render('index', {objData: "Что-то не так с запросом!!!"});
+//         }
         
 
-    } else {
-        console.warn(error);
-    }
-});
+//     } else {
+//         console.warn(error);
+//     }
+// });
     
 
 
     
+// });
+app.post('/search', function (req, res) {   
+    // res.render('index', {objData: "Поиск..."});  
+    console.log(req.body.lat, req.body.lng);  
+    res.json({response: req.body.lat});
 });
 
-app.listen(3000, function () {
-    console.log('Server started at localhost:3000');
+app.listen(3333, function () {
+    console.log('Server started at localhost:3333');
 });
